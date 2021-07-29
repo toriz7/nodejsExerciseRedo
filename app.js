d3ffd7e182f343a81d7edfd52bed1c37bf539344
toriz7 <toriz7@naver.com>
2021-07-30 01:48:23 +0900

    express templates

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const admin = require('./routes/admin');
+const nunjucks= require('nunjucks');
+
 
 const app = express();
 const port = 3000;
 
+nunjucks.configure('template',{ //폴더 지정
+    autoescape: true, //보안 이유로 설정
+    express:app //앱 설정
+})
+
+
 app.get('/', (req,res) => {
     res.send('express start');
 });
+
+app.use('/admin',admin)
 
 app.listen( port, () => {
     console.log('Express listening on port', port);
